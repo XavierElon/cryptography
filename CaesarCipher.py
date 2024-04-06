984ec00d9067c59063bc471facb7fa1603aeea7f
XavierElon <xavierh93@hotmail.com>
2024-04-05 20:45:26 -0400

    caesher cipher

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,1 +1,13 @@
 ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+KEY = 3
+
+def caesar_encrypt(plain_text):
+    cipher_text = ''
+    plain_text = plain_text.upper()
+    for c in plain_text:
+        index = ALPHABET.find(c)
+        
+        index = (index + KEY) % len(ALPHABET)
+        cipher_text = cipher_text + ALPHABET[index]
+        
+    return cipher_text

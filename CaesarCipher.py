ed2f1ddb2ee64ee96cd3017a4c484b025c85853f
XavierElon <xavierh93@hotmail.com>
2024-04-05 21:02:40 -0400

    caesar brute force

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -11,3 +11,22 @@ def caesar_encrypt(plain_text):
         cipher_text = cipher_text + ALPHABET[index]
         
     return cipher_text
+
+def caesar_decrypt(cipher_text):
+    plain_text = ''
+    cipher_text = cipher_text.upper()
+    for c in cipher_text:
+        index = ALPHABET.find(c)
+
+        index = (index - KEY) % len(ALPHABET)
+        plain_text = plain_text + ALPHABET[index]
+    
+    return plain_text
+
+if __name__ == '__main__':
+    m = 'Flocka, Cosmo, Achilles and Michelle'
+    
+    encrypted = caesar_encrypt(m)
+    print(encrypted)
+    decrypted = caesar_decrypt(encrypted)
+    print(decrypted)
---
+++ CaesarCipherBruteForce.py
@@ -0,0 +1,18 @@
+ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ,'
+
+def crack_caesar(cipher_text):
+    print(cipher_text)
+    for key in range(len(ALPHABET)):
+        plain_text = ''
+        
+        for c in cipher_text:
+            index = ALPHABET.find(c)
+            index = (index - key) % len(ALPHABET)
+            plain_text = plain_text + ALPHABET[index]
+            
+        print('With key %s, the result is: %s' % (key, plain_text))
+
+if __name__ == '__main__':
+    print('here')
+    encrypted = 'IORFNDBCFRVPRBCDFKLOOHVCDQGCPLFKHOOH'
+    crack_caesar(encrypted)

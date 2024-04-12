8b3a84b4d023cd7da512181d99888173d266cbdc
XavierElon <xavierh93@hotmail.com>
2024-04-12 14:39:53 -0400

    finish one time pad

--- otp/OneTimePad.py
+++ otp/OneTimePad.py
@@ -4,10 +4,26 @@ ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def encrypt(text, key):
     text = text.upper()
-    key = key.upper()
+    cipher_text = ''
+    
+    for index, char in enumerate(text):
+        key_index = key[index]
+        
+        char_index = ALPHABET.find(char)
+        cipher_text += ALPHABET[(char_index + key_index) % len(ALPHABET)]
+        
+    return cipher_text
 
-def decrypt(text):
-    pass
+def decrypt(cipher, key):
+    plain = ''
+    
+    for index, char in enumerate(cipher):
+        key_index = key[index]
+        char_index = ALPHABET.find(char)
+        plain += ALPHABET[(char_index - key_index) % len(ALPHABET)]
+        
+    return plain
+        
 
 def random_sequence(text):
     random = []
@@ -19,5 +35,10 @@ def random_sequence(text):
 
 if __name__ == '__main__':
     
-    message = 'This is a random text'
-    print(random_sequence(message))
+    message = 'In this course you will learn about cryptography and hashing in Python and Java as well. You will understand most of the private key (symmetric) and pubic key (asymmetric) cryptosystems on a step by step basis. You can learn about the theory as well as the implementation for every cryptographic algorithm - and how to crack these systems (so what are the weaknesses).'
+    seq = random_sequence(message)
+    print("Original message: %s" % message.upper())
+    cipher_text = encrypt(message, seq)
+    print("Encrypted message: %s" % cipher_text)
+    decrypted_text = decrypt(cipher_text, seq)
+    print("Decrypted message: %s" % decrypted_text)

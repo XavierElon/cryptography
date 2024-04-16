8c912bb88ced4e2b573ff7216b25c7372677983f
XavierElon <xavierelon93@gmail.com>
2024-04-16 09:18:06 -0400

    DiffieHellman

---
+++ assymetrict-public-private-key-cryptography/DiffieHellman.py
@@ -0,0 +1,14 @@
+import random
+
+
+def generate_private_key(n, g):
+    x= random.randint(1, n)
+    y = random.randint(1, n)
+    
+    k1 = pow(g, x, n)
+    k2 = pow(g, y, n)
+    
+    print("Alice's private key %s" % pow(k2, x, n))
+    print("Bob's private key %s" % pow(k1, y, n))
+    
+    
